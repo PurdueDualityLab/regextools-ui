7f4b803a119e26e13562b2140800b8e2e4835c09
Charlie Sale <softwaresale01@gmail.com>
2022-12-07 00:11:58 -0500

    Implement UI paging, implement regex101 integration (#1)
    * Implement paging
    
    * Implement regex101 feature

--- src/app/query/results-store.service.ts
+++ src/app/query/results-store.service.ts
@@ -1,87 +1,112 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
-import { map, Observable, switchMap, tap } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { RegexDBQuery, RegexDBResponse } from './query.model';
+import { forkJoin, map, Observable, of, switchMap, tap } from 'rxjs';
+import { withLatestFrom, } from 'rxjs/operators';
+import { RegexDBQuery } from './query.model';
+import { QueryRequest, QueryResponse, QueryService } from './query.service';
 
 export type ResultState = 'added' | 'removed' | 'same';
 
 export interface ResultsState {
   results: string[];
-  added: number[];
-  removed: string[];
-  same: number[];
+  total: number;
   loading: boolean;
+  cacheKey?: string;
+  pageSize?: number;
+  pageNum?: number;
+  pageCount?: number;
 }
+
+const resultStateFromQueryResponse = (response: QueryResponse): ResultsState => ({
+  results: response.results,
+  total: response.total,
+  loading: false,
+  cacheKey: response.cacheKey,
+  pageSize: response.pageSize,
+  pageNum: response.pageNum,
+  pageCount: response.pageCount,
+})
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResultsStoreService extends ComponentStore<ResultsState> {
 
+  /* Selectors */
   readonly isLoading$ = this.select(state => state.loading);
   readonly results$ = this.select(state => state.results);
-  readonly added$ = this.select(state => state.added.map(idx => state.results[idx]));
-  readonly removed$ = this.select(state => state.removed);
-  readonly same$ = this.select(state => state.same.map(idx => state.results[idx]));
-  readonly resultCount$ = this.select(state => state.results.length);
+  readonly cacheKey$ = this.select(state => state.cacheKey);
+  readonly pageSize$ = this.select(state => state.pageSize);
+  readonly pageNum$ = this.select(state => state.pageNum);
+  readonly pageCount$ = this.select(state => state.pageCount);
   readonly hasResults$ = this.select(state => state.results.length > 0);
+  readonly resultCount$ = this.select(state => state.total);
+  readonly pageState$ = this.select(state => ({ pageSize: state.pageSize ?? 0, pageNum: state.pageNum ?? 0, pageCount: state.total }));
 
-  readonly updateResults = this.updater((state, newResults: string[]) => {
-    if (state.results.length === 0) {
-      // fill out the new stuff when there's something to compare against
-      return {
-        ...state,
-        results: newResults,
-        same: newResults.map((_, idx) => idx)
-      };
-    }
+  /* Updaters */
+  readonly setLoading = this.updater((state, isLoading: boolean) => ({ ...state, loading: isLoading }));
+  readonly setStateFromResponse = this.updater((state, queryResponse: QueryResponse) => resultStateFromQueryResponse(queryResponse));
+  readonly clear = this.updater(() => ({ results: [], loading: false, total: 0 }));
+  readonly updatePaging = this.updater((state, pageEv: PageEvent) => ({ ...state, pageCount: pageEv.length, pageSize: pageEv.pageSize, pageNum: pageEv.pageIndex }));
 
-    // Figure out which are new
-    const addedResultsIdx = newResults.filter(res => !state.results.includes(res)).map(result => newResults.indexOf(result));
-    const removedResults = state.results.filter(res => !newResults.includes(res));
-    const sameResultsIdx = newResults.filter(result => state.results.includes(result)).map(result => newResults.indexOf(result));
-
-    return {
-      results: newResults,
-      added: addedResultsIdx,
-      removed: removedResults,
-      same: sameResultsIdx,
-      loading: false,
-    };
-  });
-
-  readonly toggleLoadingState = this.updater((state, newLoadingState: boolean) => ({ ...state, loading: newLoadingState }));
-
-  readonly clear = this.updater(() => ({ results: [], added: [], removed: [], same: [], loading: false }));
-
-  readonly query = this.effect((regexQuery$: Observable<RegexDBQuery>) => regexQuery$.pipe(
+  /* Effects */
+  readonly executeQuery = this.effect((query$: Observable<RegexDBQuery>) => query$.pipe(
+    tap(() => this.setLoading(true)),
+    withLatestFrom(this.cacheKey$, this.pageSize$, this.pageNum$),
+    map(([query, cacheKey, pageSize, pageNum]) => {
+      console.log(`ExecuteQuery: got cache key ${cacheKey}`);
+      if ([pageSize && pageNum]) {
+        // load the rest of the paging info
+        return {
+          ...query,
+          pageRequest: {
+            pageSize: pageSize!,
+            pageNum: pageNum!,
+            cacheKey: cacheKey
+          }
+        } as QueryRequest;
+      } else {
+        // We have no paging info, so don't send it over
+        return { ...query } as QueryRequest;
+      }
+    }),
     tapResponse(
-      () => this.toggleLoadingState(true), // set loading first. Gets unset when updateResults completes
-      (err) => console.log(err)
-    ),
-    switchMap(query => this.http.post<RegexDBResponse>(`${environment.apiBase}/query`, query).pipe(
-      map(response => response.results),
-      tapResponse(
-        (results) => {
-          this.toggleLoadingState(false);
-          this.updateResults(results);
-        },
-        (error) => console.log(error),
-      )
-    )),
+      request => {
+        console.log('ExecuteQuery::tapResponse - executing query...');
+        const results$ = this.queryService.execute(request);
+        console.log('ExecuteQuery::tapResponse - updating state...');
+        this.setStateFromResponse(results$); // This clears the loading flag
+        console.log('ExecuteQuery::tapResponse - done')
+      },
+      err => {
+        console.error(err)
+      },
+    )
+  ));
+
+  readonly handlePage = this.effect((pageEvent$: Observable<PageEvent>) => pageEvent$.pipe(
+    tapResponse(
+      pageEvent => {
+        console.log('Handling page event');
+        // Update the paging info held in the state
+        this.updatePaging(pageEvent);
+        // Reinvoke the query, which uses the paging info from inside the state
+        this.executeQuery({ positive: [], negative: [] });
+      },
+      err => console.error(err),
+    )
   ));
 
   constructor(
-    private readonly http: HttpClient,
+    private readonly queryService: QueryService,
   ) {
     super({
       results: [],
-      added: [],
-      removed: [],
-      same: [],
+      total: 0,
       loading: false,
+      pageSize: 10,
+      pageNum: 0
     });
   }
 }
