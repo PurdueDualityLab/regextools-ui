04f1d5e33be0afe7a41e8ea5d7dfa2ee12571708
Charlie Sale <softwaresale01@gmail.com>
2022-12-06 23:51:42 -0500

    Implement regex101 feature

--- src/app/query/results/result-item/result-item.component.ts
+++ src/app/query/results/result-item/result-item.component.ts
@@ -2,6 +2,8 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Clipboard as CdkClipboard } from '@angular/cdk/clipboard';
 import { Component, Input, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { combineLatest, forkJoin, map, Observable } from 'rxjs';
+import { QueryStoreService } from '../../query-store.service';
 import { ResultState } from '../../results-store.service';
 
 @Component({
@@ -35,6 +37,7 @@ export class ResultItemComponent implements OnInit {
   constructor(
     private readonly snackBar: MatSnackBar,
     private readonly clipboard: CdkClipboard,
+    private readonly queryStore: QueryStoreService,
   ) { }
 
   ngOnInit(): void {
@@ -42,6 +45,24 @@ export class ResultItemComponent implements OnInit {
 
   showComingSoon() {
     this.snackBar.open('Feature coming soon!', 'DISMISS', { duration: 3000 });
+  }
+
+  createRegex101Url$(): Observable<string> {
+    // First, join all of the query strings by newlines
+    return combineLatest([this.queryStore.positive$, this.queryStore.negative$])
+      .pipe(
+        map(([positive, negative]) => {
+          const positiveBlock = positive.join('\n');
+          const negativeBlock = negative.join('\n');
+
+          const allText = [positiveBlock, negativeBlock].join('\n');
+
+          const encodedRegex = encodeURIComponent(this.regex);
+          const encodedText = encodeURIComponent(allText);
+
+          return `https://regex101.com/?regex=${encodedRegex}&testString=${encodedText}`;
+        })
+      );
   }
 
   setToClipboard() {
